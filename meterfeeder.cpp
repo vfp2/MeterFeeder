7867a1b5cd760a30eb3f1c66c4729b5cb07b6e44
Simon McCorkindale <simon@funx.jp>
2020-10-19 21:25:59 +0900

    Move main() into meterfeeder.cpp

---
+++ meterfeeder.cpp
@@ -0,0 +1,34 @@
+
+/**
+ * MeterFeeder Library
+ * 
+ * by fp2.dev
+ */
+
+#include "driver.h"
+
+int main() {
+	using namespace MeterFeeder;
+	Driver* driver = new Driver();
+	string errorReason = "";
+	if (!driver->Initialize(&errorReason)) {
+		cout << errorReason << endl;
+		return -1;
+	}
+	vector<Generator>* generators = driver->GetListGenerators();
+	if (generators->size() == 0) {
+		cout << "No generators" << endl;
+		return -1;
+	}
+	for (int i = 0; i < generators->size(); i++) {
+		Generator *generator = &generators->at(i);
+		UCHAR byte;
+		driver->GetByte(generator->GetHandle(), &byte, &errorReason);
+		if (errorReason.length() == 0) {
+			cout << errorReason << endl;
+			return -1;
+		}
+		cout << generator->GetSerialNumber() << " : " << (int)byte << endl;
+	}
+	driver->Shutdown();
+}
