9adb626d1bee95f37b622b71edbaec09b67b421a
Simon McCorkindale <simon@funx.jp>
2020-10-22 20:55:54 +0900

    We have realtime graphed output of 2 MEDs!

--- parking_warden.py
+++ parking_warden.py
@@ -8,67 +8,52 @@ import platform
 from ctypes import *
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 # Config the Python<->C++ interface with MeterFeeder
-meter_feeder_lib = cdll.LoadLibrary('/Users/simon/DropboxPersonal/MMI/codeiMac/MeterFeeder/libmeterfeeder.dylib')
+meter_feeder_lib = cdll.LoadLibrary('/Users/simon/MMI/code/MeterFeeder/libmeterfeeder.dylib')
 meter_feeder_lib.Initialize.argtypes = c_char_p,
 meter_feeder_lib.Initialize.restype = c_int
 meter_feeder_lib.GetNumberGenerators.restype = c_int
 meter_feeder_lib.GetByte.argtypes = c_char_p, c_char_p,
 meter_feeder_lib.GetByte.restype = c_ubyte
 
-errorReason = create_string_buffer(256)
-
 # Initialize the driver
+errorReason = create_string_buffer(256)
 result = meter_feeder_lib.Initialize(errorReason)
 print("MeterFeeder::Initialize: result: " + str(result) + ", errorReason:", errorReason.value)
 if (len(errorReason.value) > 0):
     exit(result)
 result = meter_feeder_lib.GetNumberGenerators()
 print("MeterFeeder::GetNumberGenerators: result: " + str(result))
+# TODO: implement "MeterFeeder::GetListGenerators and dynamically get the serial numbers
 
+#
+# >>> Real-time graphing results
+#
 
-x = [2, 4, 6]
-y = [1, 3, 5]
-plt.plot(x, y)
-x = [4, 8, 12]
-y = [2, 6, 10]
-plt.plot(x, y)
+fig, ax = plt.subplots()
+y1data, y2data = [], []
+
+def update(frame):
+    plt.cla() # clear legend each frame
+
+    y1 = meter_feeder_lib.GetByte(b"QWR4M004", errorReason)
+    y1data.append(y1)
+    plt.plot(y1data, 'b-', label="QWR4M004")
+
+    y2 = meter_feeder_lib.GetByte(b"QWR4A003", errorReason)
+    y2data.append(y2)
+    plt.plot(y2data, 'r-', label="QWR4A003")
+
+    plt.legend(loc=1)
+
+ani = FuncAnimation(fig, update, interval=1)
 plt.show()
 
-# plt.axis([0, 10, 0, 1])
-
-# for i in range(10):
-#     y = np.random.random()
-#     plt.plot(i, y)
-#     plt.pause(0.5)
-
-# plt.show()
-
-
-
-
-# plt.axis([0, 100, 0, 256])
-
-# for i in range(100):
-#     y = np.random.random()
-#     result = meter_feeder_lib.GetByte("QWR4M004", errorReason)
-#     print("MeterFeeder::GetByte: result: " + str(result) + ", errorReason:", errorReason.value)
-#     y = result
-#     plt.plot(i, y)
-#     print("i:" + str(i) + ", y:" + str(y))
-#     plt.pause(0.05)
-
-
-# y = np.linspace(-10, 10, 1000)
-
-# # calculate the x value for each element of the y vector
-# x = y**2 + 2*y + 2  
-
-# fig, ax = plt.subplots()
-# ax.plot(x, y)
-
-# plt.show()
+#
+# <<< EOF Real-time graphing results
+#
 
 # Shutdown the driver
 meter_feeder_lib.Shutdown()
