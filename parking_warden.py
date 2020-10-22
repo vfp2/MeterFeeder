e7134f4ece266ae6ef1ff46f0a31ed2884079aa9
Simon Nishi McCorkindale <simon@funx.jp>
2020-10-22 09:14:43 +0900

    Graph hacking

--- parking_warden.py
+++ parking_warden.py
@@ -4,21 +4,10 @@
  # by fp2.dev
  ##
 
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# y = np.linspace(-10, 10, 1000)
-
-# # calculate the x value for each element of the y vector
-# x = y**2 + 2*y + 2  
-
-# fig, ax = plt.subplots()
-# ax.plot(x, y)
-
-# plt.show()
-
 import platform
 from ctypes import *
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Config the Python<->C++ interface with MeterFeeder
 meter_feeder_lib = cdll.LoadLibrary('/Users/simon/DropboxPersonal/MMI/codeiMac/MeterFeeder/libmeterfeeder.dylib')
@@ -38,8 +27,48 @@ if (len(errorReason.value) > 0):
 result = meter_feeder_lib.GetNumberGenerators()
 print("MeterFeeder::GetNumberGenerators: result: " + str(result))
 
-result = meter_feeder_lib.GetByte("QWR4M004", errorReason)
-print("MeterFeeder::GetByte: result: " + str(result) + ", errorReason:", errorReason.value)
+
+x = [2, 4, 6]
+y = [1, 3, 5]
+plt.plot(x, y)
+x = [4, 8, 12]
+y = [2, 6, 10]
+plt.plot(x, y)
+plt.show()
+
+# plt.axis([0, 10, 0, 1])
+
+# for i in range(10):
+#     y = np.random.random()
+#     plt.plot(i, y)
+#     plt.pause(0.5)
+
+# plt.show()
+
+
+
+
+# plt.axis([0, 100, 0, 256])
+
+# for i in range(100):
+#     y = np.random.random()
+#     result = meter_feeder_lib.GetByte("QWR4M004", errorReason)
+#     print("MeterFeeder::GetByte: result: " + str(result) + ", errorReason:", errorReason.value)
+#     y = result
+#     plt.plot(i, y)
+#     print("i:" + str(i) + ", y:" + str(y))
+#     plt.pause(0.05)
+
+
+# y = np.linspace(-10, 10, 1000)
+
+# # calculate the x value for each element of the y vector
+# x = y**2 + 2*y + 2  
+
+# fig, ax = plt.subplots()
+# ax.plot(x, y)
+
+# plt.show()
 
 # Shutdown the driver
 meter_feeder_lib.Shutdown()
