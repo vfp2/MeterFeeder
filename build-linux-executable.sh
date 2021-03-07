da0cf28518db5a94b814b0613be71dc6e27e0286
Simon Nishi McCorkindale <simon@funx.jp>
2021-03-07 22:36:45 +0800

    Fix Linux build script for exe

--- build-linux-executable.sh
+++ build-linux-executable.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # TODO: will make a nice multi-platform friendly CMakeFile or something soon :-D
 
-g++ -std=c++11 -lusb-1.0 -L./ftd2xx/linux -lftd2xx -g *.cpp -o meterfeeder
+g++ -std=c++11 -g *.cpp -o meterfeeder -lusb -L./ftd2xx/linux -lftd2xx -lpthread
