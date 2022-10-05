b9d40163351a0f28f3346812de89d6ba1b35c3f1
randonauts-llc <randonautsapi@gmail.com>
2022-10-05 23:10:08 +0100

    removed debug log

--- udev/ftdi_unbind.sh
+++ udev/ftdi_unbind.sh
@@ -6,6 +6,5 @@ DEVICEID="$1"
 while [ ! -d /sys/bus/usb/devices/$DEVICEID* ]
 do
     sleep 1
-    echo $DEVICEID > /home/flux/bound
 done
 ls /sys/bus/usb/drivers/ftdi_sio | grep $DEVICEID > /sys/bus/usb/drivers/ftdi_sio/unbind
