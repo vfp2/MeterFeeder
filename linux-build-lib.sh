7926ffb0503c83f1a53a3ea9f195ca5d858ed71f
soliax <soliax@coffee>
2022-11-22 20:48:45 +0800

    Fix Linux build g++ libusb args

--- linux-build-lib.sh
+++ linux-build-lib.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # TODO: will make a nice multi-platform friendly CMakeFile or something soon :-D
 
-g++ -std=c++11 -g ./src/*.cpp -o ./builds/linux/libmeterfeeder.so -lusb -L./ftd2xx/linux -lftd2xx -lpthread -shared -fPIC
+g++ -std=c++11 -g ./src/*.cpp -o ./builds/linux/libmeterfeeder.so -lusb-1.0 -L./ftd2xx/linux -lftd2xx -lpthread -shared -fPIC
