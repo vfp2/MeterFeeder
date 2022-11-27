e38e310511d44a2062143947b386ac7743a618ea
Wandering Consciousness <simon@funx.jp>
2022-11-27 12:26:54 +0800

    [Android] Get first-time app load reading random bytes working for multiple MEDs

--- android/MeterFeeder/app/src/main/java/dev/fp2/meterfeeder/app/MainActivity.java
+++ android/MeterFeeder/app/src/main/java/dev/fp2/meterfeeder/app/MainActivity.java
@@ -3,42 +3,92 @@ package dev.fp2.meterfeeder.app;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import dev.fp2.meterfeeder.MeterFeeder;
+import dev.fp2.meterfeeder.MeterFeederException;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvs[];
+    LinearLayout textViewsLayout;
+    TextView textViews[];
+
+    boolean isMEDsInitialized = false;
+    int numberMEDs = 0;
+    String[] meds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-        MeterFeeder.MF_Initialize(this);
-
-//        tvs = new TextView[MeterFeeder.MF_GetNumberGenerators()];
-        tvs = new TextView[2];
-        tvs[0] = (TextView) findViewById(R.id.medtv1);
-        tvs[1] = (TextView) findViewById(R.id.medtv1);
+        textViewsLayout = (LinearLayout) findViewById(R.id.text_views_layout);
 
         Button refreshBtn = (Button) findViewById(R.id.refresh_btn);
         refreshBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                refresh();
+                refreshMEDs();
             }
         });
 
-//        refresh();
+        initMEDs();
+        refreshMEDs();
     }
 
-    void refresh() {
-        String[] generators = MeterFeeder.MF_GetListGenerators();
-        for (int i = 0; i < generators.length; i++) {
-            tvs[i].setText(generators[i] + ": " + (MeterFeeder.MF_GetByte(generators[i].substring(0, 8)) & 0xff));
+    void initMEDs() {
+        try {
+            if (!isMEDsInitialized) {
+                String initStatus = MeterFeeder.MF_Initialize(this);
+                if (initStatus != null && !initStatus.isEmpty()) {
+                    Toast.makeText(this, initStatus, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                // Create a text view for each connected generator (MED) to display it's output
+                numberMEDs = MeterFeeder.MF_GetNumberGenerators();
+                textViews = new TextView[numberMEDs];
+                for (int i = 0; i < numberMEDs; i++) {
+                    TextView tv = new TextView(this);
+                    tv.setText(""+(i + 1));
+                    textViewsLayout.addView(tv);
+                    textViews[i] = tv;
+                }
+
+                // Keep a list of all the connected MEDs (Generators)
+                meds = MeterFeeder.MF_GetListGenerators();
+            }
+        } catch (MeterFeederException mfe) {
+            mfe.printStackTrace();
+            Toast.makeText(this, mfe.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        isMEDsInitialized = true;
+    }
+
+    void refreshMEDs() {
+        try {
+            if (!isMEDsInitialized) {
+                initMEDs();
+                if (!isMEDsInitialized) {
+                    return;
+                }
+            }
+
+            for (int i = 0; i < meds.length; i++) {
+                String medId = meds[i].substring(0, 8);
+                byte bite = -1;
+                int bite2 = bite;
+                bite = MeterFeeder.MF_GetByte(medId);
+                bite2 = bite & 0xff;
+                textViews[i].setText(meds[i] + ": " + bite2);
+            }
+        } catch (MeterFeederException mfe) {
+            mfe.printStackTrace();
+            Toast.makeText(this, mfe.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
 
