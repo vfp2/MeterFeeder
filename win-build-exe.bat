86d0105f544028e6c61f0479dd7ba2a1fcacd4a6
soliaxer <simon@funx.jp>
2022-12-10 17:32:56 +0800

    Switch Win EXE build from gcc to msvc

--- win-build-exe.bat
+++ win-build-exe.bat
@@ -1,3 +1,8 @@
 ::# quick hack for local windows exe build
 
-g++ -Wall -L./ftd2xx/amd64 -lftd2xx64 -g src\*.cpp -o builds\windows\meterfeeder.exe
+::# gcc method
+::# g++ -Wall -L./ftd2xx/amd64 -lftd2xx64 -g src\*.cpp -o builds\windows\meterfeeder.exe
+
+::# Microsoft C++ compiler method
+REM Check the Visual Studio PATH config in README, then run vcvars64.bat before running this script
+cl.exe /O2 src\*.cpp /MT /link /OUT:builds\windows\meterfeeder.exe ftd2xx\amd64\ftd2xx.lib
