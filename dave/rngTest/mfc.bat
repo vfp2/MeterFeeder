f67af11f2e2e7de9a86373b46e437b157961ad65
Wandering Consciousness <simon@funx.jp>
2023-06-25 13:34:00 +0800

    Update mfc.bat

--- dave/rngTest/mfc.bat
+++ dave/rngTest/mfc.bat
@@ -1,2 +1,2 @@
 g++ -c meterFeeder2.cpp driver.cpp generator.cpp
-g++ -o meterFeeder2.exe generator.o driver.o meterFeeder2.o -L"." -lftd2xx
+g++ -o meterFeeder2 generator.o driver.o meterFeeder2.o -L"." -lftd2xx
