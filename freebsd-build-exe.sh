b9f56b6b16d90108aed94d72fca58d4cdac46cc8
Wandering Consciousness <10329002+Wandering-Consciousness@users.noreply.github.com>
2024-12-25 21:44:07 +0900

    Update freebsd-build-exe.sh

--- freebsd-build-exe.sh
+++ freebsd-build-exe.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # TODO: will make a nice multi-platform friendly CMakeFile or something soon :-D
 
-g++ -std=c++11 -g ./src/*.cpp -o ./builds/freebsd/meterfeeder -lpthread -lftdi1
+g++ -std=c++11 -g ./src/*.cpp -o ./builds/freebsd/meterfeeder -lpthread -lftdi1 -lusb-1.0
